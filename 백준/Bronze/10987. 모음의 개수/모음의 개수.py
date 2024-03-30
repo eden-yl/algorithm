0130acc73f21adb9e96abc04583996d355b5e063
eden <161665058+eden-yl@users.noreply.github.com>
2024-03-31 06:29:25 +0900

    [Bronze IV] Title: 모음의 개수, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.py
@@ -0,0 +1,14 @@
+count=0
+li=list(input())
+for i in li:
+    if i=='a':
+        count += 1
+    elif i=='e':
+        count += 1
+    elif i=='i':
+        count += 1
+    elif i=='o':
+        count += 1
+    elif i=='u':
+        count += 1
+print(count)
