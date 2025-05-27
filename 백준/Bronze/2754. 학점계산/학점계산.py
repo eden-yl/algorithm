65283da987085e0fdab75d1dee7dba999b8ec1f3
eden <161665058+eden-yl@users.noreply.github.com>
2025-05-28 03:44:59 +0900

    [Bronze III] Title: 학점계산, Time: 104 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,29 @@
+gpa=input()
+score=0
+if gpa=='A+':
+    score = '4.3'
+elif gpa=='A0':
+    score = '4.0'
+elif gpa=='A-':
+    score='3.7'
+elif gpa=='B+':
+    score='3.3'
+elif gpa=='B0':
+    score='3.0'
+elif gpa=='B-':
+    score='2.7'
+elif gpa=='C+':
+    score='2.3'
+elif gpa=='C0':
+    score='2.0'
+elif gpa=='C-':
+    score='1.7'
+elif gpa=='D+':
+    score='1.3'
+elif gpa=='D0':
+    score='1.0'
+elif gpa=='D-':
+    score='0.7'
+elif gpa=='F':
+    score='0.0'
+print(score)
