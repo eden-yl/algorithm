5a979604c571d92f7c5394725d3b3e70abe99c11
eden <161665058+eden-yl@users.noreply.github.com>
2025-06-15 21:13:23 +0900

    [Bronze II] Title: 소수, Time: 108 ms, Memory: 110736 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,22 @@
+M=int(input())
+N=int(input())
+
+def isPrime(num):
+    if num<=1:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num%i==0:
+            return False
+    return True
+
+li=[]
+for i in range(M, N+1):
+    if isPrime(int(i)):
+        li.append(int(i))
+
+if len(li)==0:
+    print(-1)
+else:
+    li.sort()
+    print(sum(li))
+    print(min(li))
